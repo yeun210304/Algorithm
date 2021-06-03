c3084a656adf896aef18f41333db9f282b82066f
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-06-04 00:04:28 +0900

    1-1 Max3

--- chap01/src/chap01/Max3.java
+++ chap01/src/chap01/Max3.java
@@ -7,7 +7,7 @@ public class Max3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		/*
-		 * 순차적(concatenation)구조 : 순차적으로 여러문장(프로세스)이 순차적으로 실행되는 구조.
+		 * 순차적(concatenation)구조 : 순차적으로 여러문장(프로세스)이 실행되는 구조.
 		 * 선택(selection)구조 : () 안 식의 평가결과에 따라 프로그램의 실행흐름을 변경하는 if문.
 		 */
 		System.out.println("세 정수의 최댓값을 구합니다.");
