f84fa7beebafc47f0b219a4d0b0db0e80c8ea5ed
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-08-27 11:13:55 +0900

    Q12 + Q13

---
+++ chap01/src/chap01/Q13.java
@@ -0,0 +1,20 @@
+package chap01;
+
+public class Q13 {
+	public static void main(String[] args) {
+		System.out.print("덧셈|");
+		for (int i = 1; i <= 9; i++)
+			System.out.printf("%3d", i);
+		System.out.println("\n---+----------------------------");
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%2d |", i);
+			for(int j = 1; j <= 9; j++) {
+				System.out.printf("%3d", i + j);
+			}
+			System.out.println();
+		}
+	}
+}
+
+
