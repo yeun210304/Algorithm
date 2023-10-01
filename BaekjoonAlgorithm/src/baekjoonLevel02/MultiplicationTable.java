c5535cfedddc1f8fcc983b6746761e0e13bfa122
Yeun <80025908+yeun210304@users.noreply.github.com>
2023-10-01 16:23:53 +0900

    Create MultiplicationTable.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel02/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package baekjoonLevel02;
+
+import java.util.Scanner;
+
+// 2739	구구단
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for (int i = 1; i <= 9; i ++) {
+			System.out.printf("%d * %d = %d\n", num, i, num*i);
+		}
+	}
+}
