6ec8ff7b576959862b741075b105f3aa9173f1f4
yeun210304 <yeun210304@gmail.com>
2023-10-03 18:09:43 +0900

    Create Sum.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel03/Sum.java
@@ -0,0 +1,21 @@
+package baekjoonLevel03;
+
+import java.util.Scanner;
+
+// 	8393	합
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		int num = sc.nextInt();
+		sc.close();
+		
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		
+		System.out.print(sum);
+	} 
+}
