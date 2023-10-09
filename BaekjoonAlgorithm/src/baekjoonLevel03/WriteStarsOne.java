b744a66948b50126b73b492e0dc5912eddac810a
yeun210304 <yeun210304@gmail.com>
2023-10-09 21:38:38 +0900

    Create WriteStarsOne.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel03/WriteStarsOne.java
@@ -0,0 +1,18 @@
+package baekjoonLevel03;
+
+import java.util.Scanner;
+
+public class WriteStarsOne {
+	// 2438	별 찍기 - 1
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		String star = "";
+		
+		for (int i=0; i<t; i++) {
+			star += "*";
+			System.out.println(star);
+		}
+	}
+}
