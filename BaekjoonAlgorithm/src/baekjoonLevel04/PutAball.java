95ee3b989a170f526b9975e1ad77ca6e1f066adb
yeun210304 <yeun210304@gmail.com>
2023-10-16 23:45:42 +0900

    Create PutAball.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel04/PutAball.java
@@ -0,0 +1,20 @@
+package baekjoonLevel04;
+
+import java.util.Scanner;
+
+public class PutAball {
+
+	// 10810	공 넣기
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		double ball = Math.random();
+		
+		for (int i=0; i<n; i++) {
+			
+		}
+	}
+}
