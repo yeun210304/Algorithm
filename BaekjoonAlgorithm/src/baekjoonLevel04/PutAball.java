a7c6d6e139acf7775ba77bd9c9746c4931a9a932
Yeun <80025908+yeun210304@users.noreply.github.com>
2023-10-22 20:01:26 +0900

    Update PutAball.java

--- BaekjoonAlgorithm/src/baekjoonLevel04/PutAball.java
+++ BaekjoonAlgorithm/src/baekjoonLevel04/PutAball.java
@@ -7,14 +7,23 @@ public class PutAball {
 	// 10810	공 넣기
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
+		int[] res = new int[sc.nextInt()+1];
 		int m = sc.nextInt();
-		int n = sc.nextInt();
 		
-		double ball = Math.random();
-		
-		for (int i=0; i<n; i++) {
+		for (int a=1; a<=m; a++) {
+			int i = sc.nextInt();
+			int j = sc.nextInt();
+			int k = sc.nextInt();
 			
+			for (int b=i; b<=j; b++) {
+				res[b] = k;
+			}
+		}
+		sc.close();
+		
+		for (int i=1; i<res.length; i++) {
+			System.out.print(res[i] + " ");
 		}
 	}
 }
