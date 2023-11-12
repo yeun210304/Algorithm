0aa4116c7ad6faa6bd7416eccf9f0e0d4849accf
Yeun <80025908+yeun210304@users.noreply.github.com>
2023-11-12 20:19:55 +0900

    Update CheckAttendanceNumber.java

--- BaekjoonAlgorithm/src/baekjoonLevel04/CheckAttendanceNumber.java
+++ BaekjoonAlgorithm/src/baekjoonLevel04/CheckAttendanceNumber.java
@@ -1,9 +1,5 @@
 package baekjoonLevel04;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Scanner;
 
 public class CheckAttendanceNumber {
@@ -11,51 +7,17 @@ public class CheckAttendanceNumber {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int[] result = new int[2];
-		List<Integer> thirty = new ArrayList<Integer>();
-		
-		int[] inArr = new int[28];
-		
-		for (int i = 0; i < inArr.length; i++) {
-			inArr[i] = sc.nextInt();
-		}
-
-		for (int i = 0; i < thirty.size(); i++) {
-			thirty.add(i+1);
-		}
-		
-		for (int i = 0; i < thirty.size(); i++) {
-			for (int j = 0; j < inArr.length; j++) {
-				if (thirty.get(i) == j) {
-					thirty.remove(i);
-				}
-			}			
-		}
-		
-		System.out.println(thirty.toString());
-		
+		int[] inArr = new int[31];
 		
 		for (int i = 0; i < 28; i++) {
-			int in = sc.nextInt();
-			int who = 0;
-			int index = 0;
-			boolean same = false;
-
-			for (int j = 1; j <= 30; j++) {
-				if (in == j) {
-					same = true;
-					continue;
-				}
-			}
-			
-			if (!same) {
-				index++;
-				result[index] = who;
-			}
-		}		
+			inArr[sc.nextInt()] = 1;
+		}
+		sc.close();
 		
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
+		for (int i = 1; i < inArr.length; i++) {
+			if (inArr[i] != 1) {
+				System.out.println(i);
+			}
 		}
 	}
 }
