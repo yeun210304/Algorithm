315de12fd368db05dc64680ab36eb485d4d2a2a3
yeun210304 <yeun210304@gmail.com>
2023-11-19 23:57:50 +0900

    Create Average.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel04/Average.java
@@ -0,0 +1,32 @@
+package baekjoonLevel04;
+
+import java.util.Scanner;
+
+// 1546	평균
+public class Average {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int subjectNum = sc.nextInt();
+
+		double m = 0;
+		double sum = 0;
+		double[] subjectArr = new double[subjectNum]; 
+		
+		for (int i = 0; i < subjectNum; i++) {
+			subjectArr[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		for (int i = 0; i < subjectArr.length; i++) { 
+			m = (int) Math.max(m, subjectArr[i]);
+		}
+		for (int i = 0; i < subjectArr.length; i++) {
+			subjectArr[i] = subjectArr[i]/m*100; 
+		}
+		for (int i = 0; i < subjectArr.length; i++) {
+			 sum += subjectArr[i];
+		}
+		System.out.println(sum/subjectNum);
+	}
+}
