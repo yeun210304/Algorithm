3032f08ae657c0ae39abe5d4a09227741598f896
Yeun <80025908+yeun210304@users.noreply.github.com>
2023-12-03 21:42:25 +0900

    Create LettersAndStrings.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel05/LettersAndStrings.java
@@ -0,0 +1,16 @@
+package baekjoonLevel05;
+
+import java.util.Scanner;
+
+// 27866	문자와 문자열
+public class LettersAndStrings {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String letter = in.nextLine();
+
+		char[] arr = letter.toCharArray();
+		
+		System.out.println(arr[in.nextInt() - 1]);
+	}
+}
