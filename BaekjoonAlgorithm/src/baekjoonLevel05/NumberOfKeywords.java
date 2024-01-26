bf91b66bbb2bd9e43280cd3b6565df226e7f9ed0
yeun210304 <yeun210304@gmail.com>
2024-01-27 01:00:08 +0900

    Create NumberOfKeywords.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel05/NumberOfKeywords.java
@@ -0,0 +1,25 @@
+package baekjoonLevel05;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 1152	단어의 개수
+public class NumberOfKeywords {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String[] word = in.nextLine().split(" ");
+		in.close();
+		
+		ArrayList<String> wordArr = new ArrayList<String>();
+		
+		for (int i = 0; i < word.length; i++) {
+			if (!"".equals(word[i])) {
+				wordArr.add(word[i]);
+			}
+		}
+		
+		System.out.print(wordArr.size());
+	}
+}
