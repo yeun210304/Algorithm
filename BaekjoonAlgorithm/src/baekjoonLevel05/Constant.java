24f1315d8845d90394ac7dec65ed779236925944
yeun210304 <yeun210304@gmail.com>
2024-01-27 23:08:49 +0900

    Create Constant.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel05/Constant.java
@@ -0,0 +1,19 @@
+package baekjoonLevel05;
+
+import java.util.Scanner;
+
+// 	2908	상수
+public class Constant {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int a = Integer.parseInt(new StringBuilder(in.next()).reverse().toString());
+		int b = Integer.parseInt(new StringBuilder(in.next()).reverse().toString());
+		in.close();
+		
+		String max = Integer.toString(Math.max(a, b));
+		
+		System.out.println(max);
+	}
+}
