82e94c0e443decee91d3fcc9da1c02f4d31e7fcb
yeun210304 <yeun210304@gmail.com>
2024-02-04 01:49:35 +0900

    Stargazing7

---
+++ BaekjoonAlgorithm/src/baekjoonLevel06/Stargazing7.java
@@ -0,0 +1,37 @@
+package baekjoonLevel06;
+
+import java.util.Scanner;
+
+// 2444	별 찍기 - 7
+public class Stargazing7 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+		in.close();
+
+		String b = " ";
+		String s = "*";
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j< n-i; j++) {
+				System.out.print(b);
+			}
+			for (int j = 0; j < i*2-1; j++) {
+				System.out.print(s);
+			}
+			System.out.println();
+		}
+ 
+		for (int i = n-1; i >= 0; i--) {
+			for(int j = 0; j < n-i; j++) {
+				System.out.print(b);
+			}
+			for(int j = 0; j < i*2-1; j++) {
+				System.out.print(s);
+			}
+			System.out.println();
+		}
+	}
+}
