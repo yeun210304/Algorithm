7bbf6bf3cdd51986fdd688765fdd90770ce90f23
yeun210304 <yeun210304@gmail.com>
2024-02-24 21:21:42 +0900

    Update YourRatingIs.java

--- BaekjoonAlgorithm/src/baekjoonLevel06/YourRatingIs.java
+++ BaekjoonAlgorithm/src/baekjoonLevel06/YourRatingIs.java
@@ -8,29 +8,35 @@ public class YourRatingIs {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		String[][] score = new String[20][3];
+		double scoreSum = 0;	// 학점의 총합
+		double multiSum = 0;	// 학점 * 과목평점
 		
-		int scLen = score.length;
-		int sum = 0;
-		int pNum = 0;;
+		String[] gradeArr = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
+		double[] rateArr = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
 		
-		float rating = 0;
 		
-		for (int i = 0; i < scLen; i++) {
-			score[i][0] = in.next();
-			score[i][1] = in.next();
-			score[i][2] = in.next();
-			
-			if ("P".equals(score[i][2]) || "F".equals(score[i][2])) {
-				score[i][1] = "0";
-				pNum++;
+		for (int i = 0; i < 20; i++) {
+			in.next();	// 과목명
+			double score = in.nextDouble();	// 학점
+			String grade = in.next();	// 등급
+			double sbjRate = 0;
+					
+			// 과목평점찾기
+			for (int j = 0; j < gradeArr.length; j++) {
+				if (gradeArr[j].equals(grade)) {
+					sbjRate = rateArr[j];
+				}
 			}
 			
-			sum += Double.parseDouble(score[i][1]);
+			if ("P".equals(grade)) {
+				score = 0;
+			}
+			
+			scoreSum += score;
+			multiSum += score * sbjRate; 
 		}
-		
-		rating = sum / (scLen - pNum);
+		in.close();
 
-		System.out.println(rating);
+		System.out.printf("%.6f\n", multiSum / scoreSum);
 	}
 }
