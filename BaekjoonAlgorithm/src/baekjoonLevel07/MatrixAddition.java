90d061307f1402b725fb95b66281315527fdf73a
yeun210304 <yeun210304@gmail.com>
2024-03-02 06:08:53 +0900

    Create MatrixAddition.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel07/MatrixAddition.java
@@ -0,0 +1,36 @@
+package baekjoonLevel07;
+
+import java.util.Scanner;
+
+// 2738	행렬 덧셈
+public class MatrixAddition {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		int m = in.nextInt();
+		
+		int[][] a = new int[n][m];
+		int[][] b = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				a[i][j] = in.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				b[i][j] = in.nextInt();
+			}
+		}
+		in.close();
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(a[i][j] + b[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
