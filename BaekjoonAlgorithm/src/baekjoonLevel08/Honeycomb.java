4941733719194949bc9d0b6caa6724c0b715ee1f
Yeun <yeun210304@gmail.com>
2024-03-15 23:36:01 +0900

    Create Honeycomb.java

---
+++ BaekjoonAlgorithm/src/baekjoonLevel08/Honeycomb.java
@@ -0,0 +1,29 @@
+package baekjoonLevel08;
+
+import java.util.Scanner;
+
+// 2292	벌집
+public class Honeycomb {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        in.close();
+        int around = 1;
+        int cnt = 0;
+
+        for (int i = 0; i <= n; i++) {
+            if (i == around) {
+                cnt++;
+                if (i == 1) {
+                    around = 2;
+                    continue;
+                }
+                around = around + (6 * (cnt - 1));
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
