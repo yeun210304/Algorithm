e2923e1c79deaee1a89885d443f8de9d3b893c54
Yeun <yeun210304@gmail.com>
2024-08-18 03:48:00 +0900

    max3

---
+++ DataStructureWithAlgorithm/src/baseAlgorithm/Max3.java
@@ -0,0 +1,22 @@
+package baseAlgorithm;
+
+import java.util.Scanner;
+
+public class Max3 {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("세 정수의 최댓값을 구합니다.");
+
+        System.out.print("b의 값 : "); int b = stdIn.nextInt();
+        System.out.print("a의 값 : "); int a = stdIn.nextInt();
+        System.out.print("d의 값 : "); int c = stdIn.nextInt();
+        stdIn.close();
+
+        int max = a;
+        if (a > max) max = b;
+        if (c > max) max = c;
+
+        System.out.println("최댓값은 " + max + "입니다.");
+    }
+}
---
+++ DataStructureWithAlgorithm/src/baseAlgorithm/Max4.java
@@ -0,0 +1,22 @@
+package baseAlgorithm;
+
+import java.util.Scanner;
+
+/**
+ * 네 값의 최댓값을 구하는 메서드를 작성하시요.
+ */
+public class Max4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("max : " + max4(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+    }
+
+    public static int max4(int a, int b, int c) {
+        int max = a;
+
+        max = Math.max(max, Math.max(b, c));
+
+        return max;
+    }
+}
