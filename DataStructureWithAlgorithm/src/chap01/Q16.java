161aac8bcb54d533fd06f2e693734b3774cc243e
Yeun <80025908+yeun210304@users.noreply.github.com>
2024-08-22 16:37:23 +0900

    Update Q16.java

--- DataStructureWithAlgorithm/src/chap01/Q16.java
+++ DataStructureWithAlgorithm/src/chap01/Q16.java
@@ -7,16 +7,32 @@ public class Q16 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("n을 입력하시오 : ");
-        // int n = sc.nextInt();
+        int n = sc.nextInt();
         sc.close();
         
         spira(4);
+        npira(n);
     }
 
     static void spira(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");    
+            }
+            for (int j = 1; j <= (i-1)*2+1; j++) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void npira(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (i - 1) * 2 + 1; j++) {
+                System.out.print(i);
             }
             System.out.println();
         }
