64a11fc9b5ce9779633e05e1db97a6c115dd1bad
Yeun <80025908+yeun210304@users.noreply.github.com>
2024-10-21 18:40:44 +0900

    java.util 속 이진검색

---
+++ DataStructureWithAlgorithm/src/chap03/BinarySearchTester.java
@@ -0,0 +1,48 @@
+package chap03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearchTester {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        do {
+            System.out.print("요솟수 : ");
+            num = sc.nextInt();
+        } while (num < 1);
+        
+        int[] x = new int[num];
+
+        System.out.println("오름차순으로 입력하세요.");
+        System.out.print("x[o] : ");
+        x[0] = sc.nextInt();
+
+        for (int i = 1; i < num; i++) {
+            do {
+                System.out.printf("x[%d] : ", i);
+                x[i] = sc.nextInt();    
+            } while (x[i] < x[i-1]);
+        }
+
+        System.out.print("검색할 값 : ");
+        int ky = sc.nextInt();
+        
+        int idx = Arrays.binarySearch(x, ky);
+
+        // 중복된 ky 중 가장 앞에 값을 가져온다.
+        if (idx >= 0) {
+            while (idx > 0 && x[idx - 1] == ky) {
+                idx--;
+            }
+        }
+
+        if (idx < 0) {
+            System.out.println("그 값의 요소가 없습니다.");
+        }
+        else  {
+            System.out.printf("%d은(는) x[%d]에 있습니다.", ky, idx);
+        }
+    }
+}
---
+++ DataStructureWithAlgorithm/src/chap03/Question5/BinarySearchTester2.java
@@ -0,0 +1,21 @@
+package chap03.Question5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class BinarySearchTester2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        List<Integer> lst = new ArrayList<Integer>();
+
+        for (int i = 0; i < 7; i++) {
+            lst.add(i, random.nextInt(20));
+        }
+
+        lst.sort(null);
+        int idx = Collections.binarySearch(lst, 3);
+
+    }
+}
