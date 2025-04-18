3c53b7ece5258714ab2e08b89f0e49bffcde1299
Yeun <yeun210304@gmail.com>
2025-04-18 12:01:58 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//1000	A+B
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int firstNumber = scanner.nextInt();
+		int secondNumber = scanner.nextInt();
+		
+		System.out.print(firstNumber + secondNumber);
+	}
+}
