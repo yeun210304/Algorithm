d96bec5c263c2d525a0e2b3e942ea7fdd369126f
Yeun <yeun210304@gmail.com>
2025-04-20 10:27:17 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+//  A+B, A-B, A*B, A/B(몫), A%B(나머지)
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
