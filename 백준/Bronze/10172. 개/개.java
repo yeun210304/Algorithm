2a6529583e53bf7dff99a27b6f27f98530048d0d
Yeun <yeun210304@gmail.com>
2025-04-20 23:58:53 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("|\\_/|").append("\n")
+            .append("|q p|   /}").append("\n")
+            .append("( 0 )\"\"\"\\").append("\n")
+            .append("|\"^\"`    |").append("\n")
+            .append("||_/=\\\\__|").append("\n");
+        System.out.print(sb.toString());
+    }
+}
