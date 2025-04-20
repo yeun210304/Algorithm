dc9e7c7a7cf09ad9dde3dfc6635e9c953511591d
Yeun <yeun210304@gmail.com>
2025-04-20 23:50:17 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("\\    /\\\n")
+          .append(" )  ( ')\n")
+          .append("(  /  )\n")
+          .append(" \\(__)|\n");
+        System.out.println(sb.toString());
+     }
+}
