653c9e843399b8c0a265c55da87847407a1c2ca5
Yeun <yeun210304@gmail.com>
2025-04-23 16:17:17 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 228 ms, Memory: 18204 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	// 2439	별 찍기 - 2
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		String star = "";
+		
+		for (int i=1; i<=t; i++) {
+			String space = "";
+			star += "*";
+
+			for (int j=0; j<t-i; j++) {
+				space += " ";
+			}
+			
+			System.out.print(space);
+			System.out.println(star);
+		} 
+	}
+}
