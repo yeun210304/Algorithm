6b1d1785011d3fd93eb32a2f1f5dce2d60c5ec0b
Yeun <yeun210304@gmail.com>
2025-05-05 15:55:17 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("         ,r'\"7").append("\n")
+            .append("r`-_   ,'  ,/").append("\n")
+            .append(" \\. \". L_r'").append("\n")
+            .append("   `~\\/").append("\n")
+            .append("      |").append("\n")
+            .append("      |");
+        System.out.print(sb.toString());
+    }
+}
