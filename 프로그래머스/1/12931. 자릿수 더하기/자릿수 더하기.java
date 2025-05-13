c0fd576869fc63e229d332c4ea659be1fb4d2f16
Yeun <yeun210304@gmail.com>
2025-05-13 23:58:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 13.19 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        return Arrays.stream((n + "").split(""))
+              .mapToInt(Integer::valueOf)
+              .sum();
+    }
+}
