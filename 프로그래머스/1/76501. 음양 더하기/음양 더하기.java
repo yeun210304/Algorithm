401360743144b10764f929030442aa5d7d57592c
Yeun <yeun210304@gmail.com>
2025-05-18 15:45:45 +0900

    [level 1] Title: 음양 더하기, Time: 13.52 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        for (int i=0; i<signs.length; i++) {
+            if(!signs[i]) {
+                absolutes[i] = -(absolutes[i]);
+            }
+            System.out.print(absolutes[i]);
+        }
+        
+        return Arrays.stream(absolutes).sum();
+    }
+}
