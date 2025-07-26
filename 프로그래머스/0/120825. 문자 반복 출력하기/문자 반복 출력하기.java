7cd4698b5753b75df34a2188f026624581d79320
Yeun <yeun210304@gmail.com>
2025-07-26 22:55:31 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 11.05 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        for (char c:my_string.toCharArray()) {
+            for (int i=0; i<n; i++) {
+                answer+=c;
+            }
+        }
+        return answer;
+    }
+}
