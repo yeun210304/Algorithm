f7020c4160b7f8966bd16e232293450f2b5761e0
Yeun <yeun210304@gmail.com>
2025-08-24 20:45:09 +0900

    [level 0] Title: 홀짝 구분하기, Time: 177.66 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        
+        System.out.printf("%d is %s", n, n%2==0?"even":"odd");
+    }
+}
