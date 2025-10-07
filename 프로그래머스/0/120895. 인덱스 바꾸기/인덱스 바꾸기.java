122d223a8ee0c304c3026221d6e70ae95c821a34
Yeun <yeun210304@gmail.com>
2025-10-07 17:31:10 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 20.82 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        return my_string.substring(0, num1) 
+            + my_string.charAt(num2)
+            + my_string.substring(num1+1, num2)
+            + my_string.charAt(num1)
+            + my_string.substring(num2+1, my_string.length());
+            
+    }
+}
