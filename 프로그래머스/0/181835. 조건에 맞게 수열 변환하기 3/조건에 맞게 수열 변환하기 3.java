09f4a576814102cafdfba205c3238e7af05caa74
Yeun <yeun210304@gmail.com>
2025-12-06 20:38:41 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 13.31 ms, Memory: 185 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        return Arrays.stream(arr)
+            .map(i -> k%2 != 0 ? i*k : i+k)
+            .toArray();
+    }
+}
