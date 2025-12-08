f14e991b1313eeb4ee9f4171815895cc1cda2943
Yeun <yeun210304@gmail.com>
2025-12-09 00:56:02 +0900

    [level 0] Title: 숫자 찾기, Time: 7.29 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,10 +1,6 @@
-import java.util.List;
-import java.util.Arrays;
-
 class Solution {
     public int solution(int num, int k) {
-        List<String> lst = Arrays.asList(String.valueOf(num).split(""));
-        int idx = lst.indexOf(String.valueOf(k));
-        return idx < 0 ? idx : idx + 1;
+        int idx = String.valueOf(num).indexOf(k+"");
+        return idx>-1 ? idx+1 : idx;
     }
 }
