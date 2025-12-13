4bce3116720dcea2f1f47e5f93b61b29bda87c12
Yeun <yeun210304@gmail.com>
2025-12-13 17:22:46 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.48 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,11 +1,8 @@
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        return IntStream.range(0, 5)
-            .map(e -> num_list[e])
-            .toArray();
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
