ded7d352096302d4f629371dd25e3764f757876b
Yeun <yeun210304@gmail.com>
2025-12-21 17:00:45 +0900

    [level 0] Title: x 사이의 개수, Time: 9.84 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -1,14 +1,10 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(String myString) {        
-        int length = myString.split("x").length;
-        boolean isEndsWithX = myString.endsWith("x");
-        int[] answer = new int[isEndsWithX ? length+1 : length];
-        String[] arr = myString.split("x");
-        
-        for (int i=0; i<arr.length; i++) {
-            answer[i] = arr[i].length();
-        }
-        
-        return answer;
+        return Arrays.stream(myString.split("x", -1))
+            .map(s -> s.length())
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
