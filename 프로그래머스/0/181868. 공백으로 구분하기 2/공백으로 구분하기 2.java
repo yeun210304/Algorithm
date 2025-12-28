b1bb0ee01698efcd70c8e3f72e5cc4313754b79c
Yeun <yeun210304@gmail.com>
2025-12-28 21:27:06 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 2.66 ms, Memory: 88 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        return Arrays.stream(my_string.split(" "))
+            .filter(e -> !"".equals(e))
+            .toArray(String[]::new);
+    }
+}
