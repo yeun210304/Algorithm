e7c6b3ac685e32b6c460805eb6bfa609acc76035
Yeun <yeun210304@gmail.com>
2026-03-17 21:15:36 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -1,9 +1,9 @@
 function solution(my_strings, parts) {
     let answer = '';
     
-    for (let i = 0; i < my_strings.length; i++) {
-        answer += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
-    }
+    my_strings.forEach((val, idx) => {
+        answer += val.substring(parts[idx][0], parts[idx][1] + 1);
+    })
         
     return answer;
 }
