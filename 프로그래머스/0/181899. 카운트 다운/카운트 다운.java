4d0a9c8667df20d1a2a126d68705d75442db2b94
Yeun <yeun210304@gmail.com>
2026-03-22 16:36:35 +0900

    [level 0] Title: 카운트 다운, Time: 3.09 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,11 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num - end_num + 1];
-        
-        for (int i = start_num, j = 0; i >= end_num; i--, j++) {
-            answer[j] = i;
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(-start_num, -end_num)
+                .map(e -> -e)
+                .toArray();
     }
 }
