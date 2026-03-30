8a1e4460d6221924f15055a02df7c35439d33bb6
Yeun <yeun210304@gmail.com>
2026-03-30 23:57:40 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -3,9 +3,7 @@ class Solution {
         int[][] answer = new int[n][n];
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++){
-                answer[i][j] = i == j ? 1 : 0;
-            }
+            answer[i][i] = 1;
         }
         
         return answer;
