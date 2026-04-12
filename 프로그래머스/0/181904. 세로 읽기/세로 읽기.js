2fd2d7eaa33661be028bff597c3591a7097abde7
Yeun <yeun210304@gmail.com>
2026-04-12 17:43:57 +0900

    [level 0] Title: 세로 읽기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,6 +1,5 @@
 function solution(my_string, m, c) {
-    return Array.from({length: my_string.length}, (_, idx) => idx)
-        .filter(i => i % m == c-1)
-        .map(idx => my_string.charAt(idx))
+    return [...my_string]
+        .filter((_, i) => i % m == c-1)
         .join('');
 }
