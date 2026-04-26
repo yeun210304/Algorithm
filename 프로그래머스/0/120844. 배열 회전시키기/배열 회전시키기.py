aaf4475f6a86fee891d8ca3d636f6fc02d480226
Yeun <yeun210304@gmail.com>
2026-04-26 21:43:11 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,6 +1,12 @@
+from collections import deque;
+
 def solution(numbers, direction):
     
+    numbers = deque(numbers)
+    
     if direction == 'right':
-        return [numbers[len(numbers) - 1]] + numbers[: len(numbers) - 1: ]
+        numbers.rotate(1)
     else:
-        return numbers[1: : ] + [numbers[0]]
+        numbers.rotate(-1)
+        
+    return list(numbers);
