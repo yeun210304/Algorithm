8b8d22a787e83764f81632d2935ab1a208286041
Yeun <yeun210304@gmail.com>
2026-08-06 21:20:20 +0900

    [level 0] Title: 합성수 찾기, Time: 0.17 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,6 +1,6 @@
 function solution(n) {
 
-    return Array.from({length: n - 4 + 1}, (_, i) => i + 4)
+    return Array.from({length: n + 1}, (_, i) => i)
         .filter(i => isPrime(i))
         .length;
 }
