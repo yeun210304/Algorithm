03a13569b236bfd959c7a85cdbf2410a0faebfaa
Yeun <yeun210304@gmail.com>
2026-09-06 17:09:30 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,8 +1,3 @@
 def solution(order):
-    orderStr = str(order)
-    orderIntArr = map(lambda x: int(orderStr[x]), range(len(orderStr)))
-    
-    return len(list(filter(is_369, orderIntArr)))
-
-def is_369(num):
-    return num != 0 and num % 3 == 0
+    order = str(order)
+    return order.count('3') + order.count('6') + order.count('9')
